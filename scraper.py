d49d20860484c3b743cd15b46c68951b23048b04
afshin <afshin.kh.dev76@gmail.com>
2024-07-13 21:09:22 +0300

    UPDATE requests - add User-Agent to prevent 403 error

--- scraper.py
+++ scraper.py
@@ -137,7 +137,10 @@ class ImdbMovieScrapper(GetDataFromSourceMixin):
 
     @staticmethod
     def scape_url(url):
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.content  # Returns the raw HTML content
         else:
