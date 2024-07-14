b1bd8a440febea1da00c4b5e876261f7a054f0e9
afshin <afshin.kh.dev76@gmail.com>
2024-07-14 21:10:20 +0300

    ADD database

--- base/database.py
+++ base/database.py
@@ -1,6 +1,9 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 from decouple import config
 
@@ -12,3 +15,15 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
